6af376f977d4ecbd94cc030edde767a2af8ee6c3
Aluno <Aluno@10.85.198.36>
2024-06-06 16:23:53 -0300

    switchche

---
+++ src/swichchex01/SwitchEx1.java
@@ -0,0 +1,14 @@
+public class SwitchEx1 {
+
+    public static void main(String args[]) {
+    }
+}
+  
+
+    /**
+     * @param args the command line arguments
+     */
+ 
+        // TODO code application logic here
+    
+   
